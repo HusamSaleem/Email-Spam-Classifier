d58d48e05a7861bd069ccefef45c3638926a31b0
= <husamsaleem707@gmail.com>
2021-09-24 14:47:10 -0700

    Added homemade NB classification

---
+++ NaiveBayes.py
@@ -0,0 +1,89 @@
+import numpy as np
+from nltk.corpus import stopwords
+
+class NaiveBayes():
+    # Takes the data and parses it into spam and not spam probability
+    def fit(self, X, y):
+        self.parse_emails(X, y)
+        pass
+
+    # Initial guess * words probs
+    def predict(self, X):
+        y_pred = [0 for i in range(len(X))]
+
+        initial_spam_guess = (self._spam_words_count) / (self._spam_words_count + self._not_spam_words_count)
+        initial_not_spam_guess = (self._not_spam_words_count) / (self._not_spam_words_count + self._spam_words_count)
+
+        i = 0
+        for x in X:
+            email = self.get_as_arr(x)
+
+            spam_score = 0
+            notspam_score = 0
+
+            for word in email:
+                if not word in self._spam_words:
+                    # Laplace Smoothing
+                    spam_score += np.log(1/self._spam_words_count)
+                else:
+                    spam_score += np.log(self._spam_words[word])
+
+                if not word in self._not_spam_words:
+                    # Laplace Smoothing
+                    notspam_score += np.log(1/self._not_spam_words_count)
+                else:
+                    notspam_score += np.log(self._not_spam_words[word])
+
+            spam_score += np.log(initial_spam_guess)
+            notspam_score += np.log(initial_not_spam_guess)
+
+            if (spam_score > notspam_score):
+                y_pred[i] = 1
+            else:
+                y_pred[i] = 0
+            
+            i += 1
+        return y_pred
+
+    def get_as_arr(self, x):
+        stop_words = set(stopwords.words('english'))
+        word_tokens = x.split()
+        clean_sentence = [w for w in word_tokens if not w.lower() in stop_words]
+
+        return clean_sentence
+
+    def parse_emails(self, X, y):
+        stop_words = set(stopwords.words('english'))
+
+        # Contain the word and their probability
+        self._spam_words = {}
+        self._not_spam_words = {}
+
+        i = 0
+        for email in X:
+            clean_sentence = self.get_as_arr(email)
+
+            for word in clean_sentence:
+                if (y[i] == 0):
+                   if not word in self._not_spam_words:
+                        self._not_spam_words[word] = 1
+                   else:
+                        self._not_spam_words[word] += 1
+                else:
+                    if not word in self._spam_words:
+                        self._spam_words[word] = 1
+                    else:
+                        self._spam_words[word] += 1
+            
+            i += 1
+        self._spam_words_count = len(self._spam_words)
+        self._not_spam_words_count = len(self._not_spam_words)
+
+        self.calculate_word_probability()
+    
+    def calculate_word_probability(self):
+        for word in self._not_spam_words:
+            self._not_spam_words[word] = (self._not_spam_words[word] / self._not_spam_words_count)
+
+        for word in self._spam_words:
+            self._spam_words[word] = (self._spam_words[word] / self._spam_words_count)
--- Spam Classifier.py
+++ Spam Classifier.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import email
+import numpy as np
 import csv
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
+from NaiveBayes import NaiveBayes as NB
 
 true_label = [0 for i in range(2501)]
 
@@ -17,7 +17,7 @@ def parse_label():
              true_label[i] = int(type)
              i += 1
     
-def read_file(filename, id):
+def read_file(filename, id, t):
     with open(filename, encoding='latin-1') as fp:
         msg = email.message_from_file(fp)
         payload = msg.get_payload()
@@ -29,44 +29,62 @@ def read_file(filename, id):
         sub = msg.get('subject')
         sub = str(sub)
         
-        write_to_csv(sub,payload,true_label[id])
+        write_to_csv(sub,payload,true_label[id], t)
 
-def write_to_csv(sub, payload, label):
-    with open("emails.csv", "a", encoding="utf-8") as f:
+def write_to_csv(sub, payload, label, t):
+    if t == "TT":
+        name = "test.csv"
+    else:
+        name = "emails.csv"
+
+    with open(name, "a", encoding="utf-8") as f:
         payload = payload.replace('\n', "")
         writer = csv.writer(f)
         writer.writerow([sub, payload, label])
 
-def write_header():
+def write_header(t):
+    if t == "TT":
+        name = "test.csv"
+    else:
+        name = "emails.csv"
+
     header = ["subject", "email", "class"]
-    with open("emails.csv", "w", encoding="utf-8") as f:
+    with open(name, "w", encoding="utf-8") as f:
         writer = csv.writer(f)
         writer.writerow(header)
 
 def parse_emails():
     parse_label()
-    write_header()
+    write_header("TR")
+    write_header("TT")
 
     for i in range(1, 2501):
         path = ".\TR\TRAIN_%d.eml" % i
-        read_file(path, i - 1)
+        read_file(path, i - 1, "TR")
+        
+    for i in range(1, 1828):
+        path = ".\TT\TEST_%d.eml" % i
+        read_file(path, i - 1, "TT")
 
 
 if __name__ == "__main__":
     parse_emails()
     
     data = pd.read_csv("./emails.csv")
+    #test = pd.read_csv("./test.csv")
 
-    # Create a vocabulary
-    vectorizer = CountVectorizer(stop_words='english')
-    all_features = vectorizer.fit_transform(data['email'])
+    X_train, X_test, y_train, y_test = train_test_split(data['email'], data['class'], test_size=0.3)
+    clf = NB()
+    clf.fit(X_train, np.array(y_train))
+    y_pred = clf.predict(X_test)
 
-    # Split the data
-    email_train, email_test, class_train, class_test = train_test_split(all_features, data['class'], test_size=0.3)
+    print(classification_report(y_test, y_pred))
+    print(confusion_matrix(y_test, y_pred))
 
-    # Train and predict
-    clf = MultinomialNB()
-    clf.fit(email_train, class_train)
-    predictions = clf.predict(email_test)
-    print(classification_report(class_test, predictions))
-    print(confusion_matrix(class_test, predictions))
+    '''
+    with open("testLabels.csv", "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(["id", "prediction"])
+        for i in range(1, len(y_pred) + 1):
+            writer.writerow([i, y_pred[i-1]])
+   '''
